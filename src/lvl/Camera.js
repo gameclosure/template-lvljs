a8d293598a6f2f3cced66a5e46e3c68a0c4e052e
Jimmy Griffith <rogueSkib@gmail.com>
2015-09-16 22:26:18 -0700

    thoughts on camera API ... TODO

--- src/lvl/Camera.js
+++ src/lvl/Camera.js
@@ -1,5 +1,8 @@
 
 exports = Class("Camera", function () {
+  // private state
+  var _properties = {};
+
   this.init = function () {
     this.x = 0;
     this.y = 0;
@@ -8,27 +11,36 @@ exports = Class("Camera", function () {
     this.zoom = 1;
   };
 
-  // Object.defineProperty(this, 'width', {
-  //   enumerable: true,
-  //   get: function () {
-  //     return this.model.width || 2 * this.model.radius || 0;
-  //   },
-  //   set: function (value) {
-  //     if (this.model.radius !== undefined) {
-  //       this.model.radius = value / 2;
-  //     } else {
-  //       this.model.width = value;
-  //     }
-  //   }
-  // });
+  Object.defineProperty(this, 'width', {
+    enumerable: true,
+    get: function () {
+      return _properties.width;
+    },
+    set: function (value) {
+      // TODO: should camera width modify backend views ...
+      // camera width === device width, so changing that would essentially scale the view to fit?
+      // alternatively, we don't allow this to change at all
+      _properties.width = value;
+    }
+  });
 
+  Object.defineProperty(this, 'height', {
+    enumerable: true,
+    get: function () {
+      return _properties.height;
+    },
+    set: function (value) {
+      // TODO: see width above
+      _properties.height = value;
+    }
+  });
 
   this.autoScrollBy = function (dx, dy) {
     // TODO: camera should have its own state
     backend.autoScrollCameraBy(dx, dy);
   };
 
-  // process changes to camera state by applying them to the backend
+  // process changes to camera state by applying them to the backend?
   this.tick = function () {
 
   };
