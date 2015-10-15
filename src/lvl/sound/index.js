cdf2f88e82ce2482b5e9b1726d78993ce29ca140
Jimmy Griffith <rogueSkib@gmail.com>
2015-10-15 14:42:17 -0700

    mute functionality in lvl.sound

--- src/lvl/sound/index.js
+++ src/lvl/sound/index.js
@@ -4,25 +4,24 @@ var soundsCache = {};
 
 exports.playMusic = function (resource, opts) {
   var opts = opts || {};
-
   if (resource.getType() !== 'music') {
     throw new Error("playMusic requires resource of type music");
   }
 
   var path = resource.getFullPath();
-  var music = soundsCache[path];
-  if (!music) {
+  var sound = soundsCache[path];
+  if (!sound) {
       soundManager.addSound(path, merge({
         background: true
       }), resource.getOpts());
-      music = soundsCache[path] = soundManager.getSound(path);
-  } else if (!music.isBackgroundMusic) {
+      sound = soundsCache[path] = soundManager.getSound(path);
+  } else if (!sound.isBackgroundMusic) {
     throw new Error("Audio file already loaded as sound; Audio files can only" +
                     "be played as either sound or music, but not both");
   }
 
   if (!isNaN(opts.volume)) {
-    music.setVolume(opts.volume);
+    sound.setVolume(opts.volume);
   }
 
   soundManager.play(path);
@@ -30,7 +29,6 @@ exports.playMusic = function (resource, opts) {
 
 exports.playSound = function (resource, opts) {
   var opts = opts || {};
-
   if (resource.getType() !== 'sound') {
     throw new Error("playSound requires resource of type sound");
   }
@@ -52,14 +50,14 @@ exports.playSound = function (resource, opts) {
   soundManager.play(path);
 };
 
-exports.mute = function (mute) {
-  throw new Error("TODO");
+exports.muteAll = function (mute) {
+  soundManager.setMuted(mute);
 };
 
 exports.muteSound = function (mute) {
-  throw new Error("TODO");
+  soundManager.setEffectsMuted(mute);
 };
 
 exports.muteMusic = function (mute) {
-  throw new Error("TODO");
+  soundManager.setMusicMuted(mute);
 };
